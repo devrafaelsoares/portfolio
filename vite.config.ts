0cd202b208a393d9314a54a7fc7b06217c75b843
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-02-25 13:39:42 -0300

    feat: add a new alias for import

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [react()],
-})
+    resolve: {
+        alias: {
+            '@': resolve(__dirname, 'src'),
+        },
+    },
+    plugins: [react()],
+});
