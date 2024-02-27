17a50c5e8df64dfd75df60a0095504574edc74da
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-02-27 20:09:51 -0300

    feat: limit the number of projects viewed

--- src/components/Main/components/Projects/index.tsx
+++ src/components/Main/components/Projects/index.tsx
@@ -1,9 +1,12 @@
-import { ReactElement } from 'react';
+import { ReactElement, useState } from 'react';
 import Reveal from '@/components/util/Reveal';
 import { CardProject } from '@/components/CardProject';
 import { projects } from '@/data/projects';
 
 export default function Index(): ReactElement {
+    const [expanded, setExpanded] = useState(false);
+    const projectsForDisplay = expanded ? projects : projects.slice(0, 3);
+
     return (
         <section data-section className="section main__projects" id="projects">
             <Reveal delay={0.8}>
@@ -14,39 +17,53 @@ export default function Index(): ReactElement {
             <div className="section__projects">
                 <Reveal delay={0.9}>
                     <section className="project card">
-                        {projects.map(({ developer, date, image, title, url, description, technologies }, index) => {
-                            const delay = (index + 12) / 10;
-                            return (
-                                <Reveal delay={delay}>
-                                    <CardProject.Root tabIndex={0} key={index} className="card__wrapper" banner={image}>
-                                        <CardProject.Date date={date} />
-                                        <CardProject.Body>
-                                            <div className="content">
-                                                <span className="author">{developer.username}</span>
-                                                <h1 className="title">
-                                                    <a href={url} target="_blank">
-                                                        {title}
+                        {projectsForDisplay.map(
+                            ({ developer, date, image, title, url, description, technologies }, index) => {
+                                const delay = (index + 12) / 10;
+                                return (
+                                    <Reveal delay={delay}>
+                                        <CardProject.Root
+                                            tabIndex={0}
+                                            key={index}
+                                            className="card__wrapper"
+                                            banner={image}
+                                        >
+                                            <CardProject.Date date={date} />
+                                            <CardProject.Body>
+                                                <div className="content">
+                                                    <span className="author">{developer.username}</span>
+                                                    <h1 className="title">
+                                                        <a href={url} target="_blank">
+                                                            {title}
+                                                        </a>
+                                                    </h1>
+                                                    <p className="text">{description}</p>
+                                                    <div className="skills">
+                                                        {technologies?.map((technologie, index) => (
+                                                            <img
+                                                                key={index}
+                                                                className="skills__skill"
+                                                                src={technologie.url}
+                                                            ></img>
+                                                        ))}
+                                                    </div>
+                                                    <a href={url} target="_blank" className="button">
+                                                        Ver mais
                                                     </a>
-                                                </h1>
-                                                <p className="text">{description}</p>
-                                                <div className="skills">
-                                                    {technologies?.map((technologie, index) => (
-                                                        <img
-                                                            key={index}
-                                                            className="skills__skill"
-                                                            src={technologie.url}
-                                                        ></img>
-                                                    ))}
                                                 </div>
-                                                <a href={url} target="_blank" className="button">
-                                                    Ver mais
-                                                </a>
-                                            </div>
-                                        </CardProject.Body>
-                                    </CardProject.Root>
-                                </Reveal>
-                            );
-                        })}
+                                            </CardProject.Body>
+                                        </CardProject.Root>
+                                    </Reveal>
+                                );
+                            }
+                        )}
+                        {projects.length > 3 && (
+                            <Reveal delay={1.3}>
+                                <button onClick={() => setExpanded(!expanded)} className="more-projects-button">
+                                    {expanded ? 'Menos' : 'Mais'} projetos
+                                </button>
+                            </Reveal>
+                        )}
                     </section>
                 </Reveal>
             </div>
