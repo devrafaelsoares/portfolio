0a8a001a25caaf162b05c03713ee38c0b3787426
devrafaelsoares <rafael.soares.developer@gmail.com>
2024-02-28 06:15:27 -0300

    feat: add new projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -7,6 +7,7 @@ import {
     PYTHON,
     REACT,
     REDIS,
+    SASS,
     SPRINGBOOT,
     TAILWINDCSS,
     TYPESCRIPT,
@@ -46,5 +47,28 @@ export const projects: Project[] = [
         date: new Date('2024-01-03 00:00:00'),
         url: 'https://github.com/devrafaelsoares/store-api-restful',
         technologies: [JAVA, SPRINGBOOT, POSTGRESQL, REDIS, DOCKER],
+    },
+    {
+        developer,
+        title: 'Github Profiles App',
+        description: 'Aplicação Web que permiti a busca das informações de um usuário existente no Github',
+        image: {
+            url: 'projects/github-profiles-app.png',
+        },
+        date: new Date('2024-02-08 00:00:00'),
+        url: 'https://github.com/devrafaelsoares/github-profiles-app',
+        technologies: [TYPESCRIPT, REACT, SASS, DOCKER],
+    },
+    {
+        developer,
+        title: 'Todo List App',
+        description:
+            'Aplicação Web que realizar o gerenciamentos de tarefas. Permitindo a criação, edição e conclusão de suas tarefas',
+        image: {
+            url: 'projects/todo-list-app.png',
+        },
+        date: new Date('2024-02-25 00:00:00'),
+        url: 'https://github.com/devrafaelsoares/todo-list',
+        technologies: [TYPESCRIPT, REACT, SASS, DOCKER],
     },
 ];
